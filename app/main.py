33a9472739cb0d2f9c6efdaceef7b1547ea19cbd
Agrim <agrimdubey37@gmail.com>
2026-01-05 19:10:00 +0530

    Changed  main.py to implement lifespan management and add CORS middleware

--- app/main.py
+++ app/main.py
@@ -1,9 +1,28 @@
 from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
+from contextlib import asynccontextmanager
+from app.routes import websocket
+from app.rabbitmq_consumer import start_consumer_thread
+from app.database import engine, Base
 
+Base.metadata.create_all(bind=engine)
 
-app = FastAPI(title = "notification_service")
- 
+@asynccontextmanager
+async def lifespan(app: FastAPI):
+    start_consumer_thread()
+    
+    yield  
+
+app = FastAPI(title="notification_service", lifespan=lifespan)
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
+app.include_router(websocket.router)
+
 @app.get("/")
 async def root():
-    return {"message":"Notification service for agile "}
-
+    return {"message": "Notification service for agile"}
