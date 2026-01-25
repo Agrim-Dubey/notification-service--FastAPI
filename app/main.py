ffcd3efdff900a8b44a301a8a1246dd38c9cc9b8
Agrim <agrimdubey37@gmail.com>
2026-01-25 16:58:09 +0530

    Flushed out old rabbitmq adhering architecture

--- app/main.py
+++ app/main.py
@@ -1,7 +1,7 @@
 from fastapi import FastAPI
 from fastapi.middleware.cors import CORSMiddleware
 from contextlib import asynccontextmanager
-from app.routes import websocket
+from app.routes import websockets, notifications
 from app.rabbitmq_consumer import start_consumer_thread
 from app.database import engine, Base
 
@@ -21,8 +21,8 @@ app.add_middleware(
     allow_methods=["*"],
     allow_headers=["*"],
 )
-app.include_router(websocket.router)
-
+app.include_router(websockets.router)
+app.include_router(notifications.router)
 @app.get("/")
 async def root():
     return {"message": "Notification service for agile"}
